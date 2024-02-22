a04ce004513e44b34f8957a34fced05582578d80
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-02-22 17:56:53 +0530

    register

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -85,7 +85,7 @@ const Register = () => {
               <div>
                 <input
                   type="email"
-                  placeholder="zk@gmail.com"
+                  placeholder="aditya@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
