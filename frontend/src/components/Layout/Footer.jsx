03a046f7b4677749c3c33546711e248fc9ecdba5
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-02-25 10:05:17 +0530

    footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By CodeWithZeeshu.</div>
+      <div>&copy; All Rights Reserved By Aditya Raj</div>
       <div>
         <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
           <FaFacebookF />
