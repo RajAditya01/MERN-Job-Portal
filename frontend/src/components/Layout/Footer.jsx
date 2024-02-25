ea69b61b46ca60632f90cf54cc34dc5609c86ca0
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-02-25 10:17:09 +0530

    footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,26 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../../main";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import {  FaLinkedin, FaGithub } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   const { isAuthorized } = useContext(Context);
+
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By Aditya Raj</div>
+      <div>&copy; All Rights Reserved By Aditya Raj.</div>
       <div>
-        <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
-          <FaFacebookF />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
-          <FaYoutube />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
+        <Link to={"https://www.linkedin.com/in/aditya-raj-aa923721a/"} target="_blank">
           <FaLinkedin />
         </Link>
-        <Link to={"https://www.instagram.com/z_4_zeeshuuu/"} target="_blank">
+        <Link to={"https://www.instagram.com/_aadii.01/"} target="_blank">
           <RiInstagramFill />
+        </Link>
+        <Link to={"https://github.com/RajAditya01"} target="_blank">
+          <FaGithub />
         </Link>
       </div>
     </footer>
